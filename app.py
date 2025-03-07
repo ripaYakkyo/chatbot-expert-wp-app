52f08d59872e1f56cab22c648ed3455437ebb676
marco arimattei <marco.arimattei@gmail.com>
2025-03-07 12:32:30 +0100

    bog

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import random
 import streamlit as st
 
-from src.config import IS_DEBUG, EXPERTS, HEADERS
+from src.config import IS_DEBUG, EXPERTS
 from src import auth, utils
 
 load_dotenv(override=True)
@@ -18,8 +18,10 @@ elif not auth.check_password():
     print("Password incorrect, stopping the script.")
     st.stop()  # Do not continue if check_password is not True.
 
-st.title("Yakkyofy Experts Interface")
 
+# ---------------------------- APP INTERFACE ----------------------------
+
+st.title("Yakkyofy Experts Interface")
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -30,7 +32,9 @@ if "sessionIds" not in st.session_state:
     st.session_state["sessionIds"] = {name: str(_id) for _id, name in zip(ids, EXPERTS.keys())}
     st.session_state["alltime_sessionIds"] = set(st.session_state["sessionIds"].values())
 
-# create a tab for each chatbot
+
+# ---------------------------- CHATBOT INTERFACE ----------------------------
+
 tabs = st.tabs(EXPERTS.keys())
 
 for (chatbot_name, chatbot_data), tab in zip(EXPERTS.items(), tabs):
@@ -38,30 +42,31 @@ for (chatbot_name, chatbot_data), tab in zip(EXPERTS.items(), tabs):
     with tab:
 
         # add the description of the chatbot
-        st.write(chatbot_data["description"])
+        tab.write(chatbot_data["description"])
 
         # add the link to the workflow
-        st.markdown(f"**Url**: [n8n WorkFlow]({chatbot_data['workflow_url']})")
-
+        tab.markdown(f"**Url**: [n8n WorkFlow]({chatbot_data['workflow_url']})")
 
         # add a button to rest sessionId and chat history
-        if st.button("Reset chat history", key=f"reset_{chatbot_name}"):
+        if tab.button("Reset chat history", key=f"reset_{chatbot_name}"):
             utils.reset_chat(chatbot_name)
 
-        # React to user input
-        if prompt := st.chat_input("What is up?", key=f"chat_input_{chatbot_name}"):
+        # Display chat messages from history on app rerun
+        for message in st.session_state["messages"][chatbot_name]:
+            tab.chat_message(message["role"]).markdown(message["content"])
+
+        if user_input := tab.chat_input("What is up?", key=f"chat_input_{chatbot_name}"):
 
             # Display user message in chat message container
-            st.chat_message("user").markdown(prompt)
+            tab.chat_message("user").markdown(user_input)
 
             # Add user message to chat history
-            st.session_state["messages"][chatbot_name].append({"role": "user", "content": prompt})
+            st.session_state["messages"][chatbot_name].append({"role": "user", "content": user_input})
 
             # Call chatbot
             response = utils.call_chatbot(
                 chatbot_data["webhook"],
-                params={"text": prompt, "sessionId": st.session_state["sessionIds"][chatbot_name]},
-                headers=HEADERS
+                params={"text": user_input, "sessionId": st.session_state["sessionIds"][chatbot_name]},
             )
 
             if response is None:
@@ -73,10 +78,5 @@ for (chatbot_name, chatbot_data), tab in zip(EXPERTS.items(), tabs):
             st.session_state["messages"][chatbot_name].append({"role": "assistant", "content": response})
 
             # Display assistant response in chat message container
-            with st.chat_message("assistant"):
-                st.markdown(response)
+            tab.chat_message("assistant").markdown(response)
 
-        # Display chat messages from history on app rerun in reverse order
-        for message in reversed(st.session_state["messages"][chatbot_name]):
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,19 @@
 from typing import Dict, Any, Union
+from time import sleep
 import requests
 import random
 import streamlit as st
 
+from src.config import HEADERS
 
-def call_chatbot(url: str, params: Dict[str, Any]={}, body: Dict[str, Any]=None, headers: Dict[str, Any]={}) -> Union[str, None]:
+
+def call_chatbot(url: str, params: Dict[str, Any]={}, body: Dict[str, Any]=None) -> Union[str, None]:
+    # sleep(1.5)
+    # return "test response"
 
     print(f"Calling {url} with params: {params} and body: {body}")
     try:
-        response = requests.get(url, params=params, json=body, headers=headers)
+        response = requests.get(url, params=params, json=body, headers=HEADERS)
 
         if response.status_code != 200:
             print(f"Error calling {url}: {response.status_code} - {response.text[:100]}")
@@ -17,12 +22,13 @@ def call_chatbot(url: str, params: Dict[str, Any]={}, body: Dict[str, Any]=None,
         if "output" not in response.json():
             print(f"Error calling {url}: {response.json()} - NO OUTPUT FOUND")
             return None
-    
+
         return response.json()["output"]
 
     except Exception as e:
         print(f"Error calling {url}: {e}")
         return None
+
 
 
 def reset_chat(chatbot_name: str) -> None:
@@ -38,3 +44,37 @@ def reset_chat(chatbot_name: str) -> None:
     st.session_state["alltime_sessionIds"].add(st.session_state["sessionIds"][chatbot_name])
 
     return None
+
+
+# def on_chat_input(chatbot_name: str, chatbot_data: dict) -> None:
+
+#     # retrieve the user_input from the chat_input widget
+#     user_input = st.session_state[f"chat_input_{chatbot_name}"]
+
+#     # Display user message in chat message container
+#     st.chat_message("user").markdown(user_input)
+
+#     # Add user message to chat history
+#     st.session_state["messages"][chatbot_name].append({"role": "user", "content": user_input})
+
+#     # Call chatbot
+#     # response = call_chatbot(
+#     #     chatbot_data["webhook"],
+#     #     params={"text": user_input, "sessionId": st.session_state["sessionIds"][chatbot_name]},
+#     #     headers=HEADERS
+#     # )
+#     sleep(1.5)
+#     response = "This is a test response"
+
+#     if response is None:
+#         response = "Sorry, an error occured. Please try again refreshing the app."
+#     else:
+#         print(f"Response from chatbot: {response} - sessionId: {st.session_state['sessionIds'][chatbot_name]}")
+
+#     # Add assistant response to chat history
+#     st.session_state["messages"][chatbot_name].append({"role": "assistant", "content": response})
+
+#     # Display assistant response in chat message container
+#     st.chat_message("assistant").markdown(response)
+
+#     return None
